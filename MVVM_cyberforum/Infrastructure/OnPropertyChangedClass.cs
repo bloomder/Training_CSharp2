674ec229e9ce6e324f94d957d9c7864072468946
Rinat <bloomder@mail.ru>
2019-10-31 18:48:58 +0300

    add solution

---
+++ MVVM_cyberforum/Infrastructure/OnPropertyChangedClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_cyberforum.Infrastructure
+{
+    class OnPropertyChangedClass
+    {        
+
+    }
+}
---
+++ MVVM_cyberforum/Infrastructure/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_cyberforum.Infrastructure
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _onExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _onExecute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object parametr) => _canExecute == null ? true : _canExecute.Invoke(parametr);
+        public void Execute(object parametr) => _onExecute.Invoke(parametr);
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
