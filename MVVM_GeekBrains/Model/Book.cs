9daa8d1060b3bf399a39bab815ff984621ec5b85
Rinat <bloomder@mail.ru>
2019-11-12 20:33:12 +0300

    implementation class InotifyPropertyChanged

--- MVVM_GeekBrains/Model/Book.cs
+++ MVVM_GeekBrains/Model/Book.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVM_GeekBrains.Model
 {
-    class Book
+    public class Book : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
