ee41c7a34e760abc436fa0e656a30ac1447f028f
Rinat <bloomder@mail.ru>
2019-11-12 20:28:08 +0300

    change namespace 16 row

--- MVVM_GeekBrains/View/MainWindow.xaml.cs
+++ MVVM_GeekBrains/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MVVM_GeekBrains
+namespace MVVM_GeekBrains.View
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
