e2b6c1f582542400972118dc98e1f8627439ba30
Rinat <bloomder@mail.ru>
2019-11-12 20:48:49 +0300

    implementation class INotifyPropertyChanged

--- MVVM_GeekBrains/ViewModel/MainWindowViewModel.cs
+++ MVVM_GeekBrains/ViewModel/MainWindowViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVM_GeekBrains.ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
