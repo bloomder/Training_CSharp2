54c37ad7b29342ea1aecc2e1cfc81a35af673a43
Rinat <bloomder@mail.ru>
2019-11-12 20:43:58 +0300

    Create Property for class Book

--- MVVM_GeekBrains/Model/Book.cs
+++ MVVM_GeekBrains/Model/Book.cs
@@ -9,6 +9,35 @@ namespace MVVM_GeekBrains.Model
 {
     public class Book : INotifyPropertyChanged
     {
+        string author;
+        string title;
+        public string Author
+        {
+            get { return author; }
+            set 
+            { 
+                author = value;
+                /*
+                if (PropertyChanged != null)/// Чтобы не дублировать код
+                    PropertyChanged(this, new PropertyChangedEventArgs("Author"));// вынесем это все в отдельный метод.
+                */
+                OnPropertyChanged("Author");
+            }
+        }
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
         public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
