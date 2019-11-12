08188c061edd98d93c5068a075b2bbfdc15f4c74
Rinat <bloomder@mail.ru>
2019-11-12 20:17:31 +0300

    Add new solution. MVVM Example

--- MVVM_GeekBrains/MainWindow.xaml.cs
+++ MVVM_GeekBrains/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MVVM_GeekBrains.View
+namespace MVVM_GeekBrains
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
