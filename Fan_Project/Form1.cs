620d88ab6b37b2d4da80fff01929380c838b5a2d
Rinat <bloomder@mail.ru>
2019-11-26 15:15:45 +0300

    add changed

--- Fan_Project/Form1.cs
+++ Fan_Project/Form1.cs
@@ -1,18 +1,23 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace Fan_Project
 {
     public partial class Form1 : Form
     {
+        [DllImport("Cimwin32.dll")]
+        static extern uint SetSpeed(in UInt64 sp);
         public Form1()
         {
             InitializeComponent();
-            label1.Text = trackBar1.Value.ToString();
+            label1.Text = trackBar1.Value.ToString();            
         }
 
         private void trackBar1_Scroll(object sender, System.EventArgs e)
         {
-            label1.Text = trackBar1.Value.ToString();            
+            label1.Text = trackBar1.Value.ToString();
+            SetSpeed(Convert.ToUInt64(trackBar1.Value));
         }
     }
 }
