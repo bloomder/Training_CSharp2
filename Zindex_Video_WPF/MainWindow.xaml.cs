355d6b4a2d48605bef0b66eb3ffd80d1c72aba2e
Rinat <bloomder@mail.ru>
2019-11-27 17:47:30 +0300

    add changes

---
+++ Zindex_Video_WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zindex_Video_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ME.Play();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+        }
+
+        private void ME_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = (MediaElement)sender;
+            mediaElement.Stop();
+            mediaElement.Play();
+        }
+    }
+}
