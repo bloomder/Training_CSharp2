f1d8804334cfb6190f3b3cd7bac94f18176e44f6
Rinat <bloomder@mail.ru>
2019-11-28 21:55:09 +0300

    add changes

--- UserControl_WPF/UserControls/MediaControl.xaml.cs
+++ UserControl_WPF/UserControls/MediaControl.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UserControl_WPF.UserControls
 {
@@ -24,9 +13,10 @@ namespace UserControl_WPF.UserControls
         public MediaControl()
         {
             InitializeComponent();
-            path = Environment.CurrentDirectory;
+            path = Environment.CurrentDirectory;            
             path += "/Theme/1.mp4";
             ME.Source = new Uri(path);
+            MessageBox.Show(ME.Source.ToString());
             ME.Play();
         }
 
