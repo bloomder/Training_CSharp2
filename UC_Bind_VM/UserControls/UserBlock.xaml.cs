30cf722edfab3d06310b8e70c7c75a796a9ea8ae
Rinat <bloomder@mail.ru>
2019-12-06 20:50:03 +0300

    add

--- UC_Bind_VM/UserControls/UserBlock.xaml.cs
+++ UC_Bind_VM/UserControls/UserBlock.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace UC_Bind_VM.UserControls
 {
@@ -11,5 +12,31 @@ namespace UC_Bind_VM.UserControls
         {
             InitializeComponent();
         }
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(UserBlock), new PropertyMetadata(""));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(UserBlock), new PropertyMetadata(""));
+
+
+
     }
 }
---
+++ UC_Bind_VM/UserControls/UserPercent.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UC_Bind_VM.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPercent.xaml
+    /// </summary>
+    public partial class UserPercent : ContentControl
+    {
+        public UserPercent()
+        {
+            InitializeComponent();
+        }       
+
+        public int Percent
+        {
+            get { return (int)GetValue(PercentProperty); }
+            set { SetValue(PercentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Percent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PercentProperty =
+            DependencyProperty.Register("Percent", typeof(int), typeof(UserPercent), new PropertyMetadata(0));        
+    }
+}
