98586bc8d464369d709a340ef48ca429cd476257
Rinat <bloomder@mail.ru>
2020-01-10 18:30:35 +0300

    add

---
+++ Project1_Con/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object b = new object();
+            b = 0;
+            int a = (int)b; a = 100;
+            char c = (char)b; c ='a';
+            Console.ReadLine();
+        }
+    }
+}
