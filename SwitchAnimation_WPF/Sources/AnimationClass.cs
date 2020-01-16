45d02ba2f7561ac603e889821872c4bcffe513ed
Rinat <bloomder@mail.ru>
2020-01-16 17:51:26 +0300

    add

---
+++ SwitchAnimation_WPF/Sources/AnimationClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchAnimation_WPF.Sources
+{
+    public class AnimationClass
+    {
+    }
+}
