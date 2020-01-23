0e1b3229a34b2e16e1eff511e0abdeaa0636a095
Rinat <bloomder@mail.ru>
2020-01-23 20:06:24 +0300

    add

---
+++ SwithAnimation_WPF/Controls/UserSwitch1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SwithAnimation_WPF.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для UserSwitch1.xaml
+    /// </summary>
+    public partial class UserSwitch1 : ContentControl
+    {
+        public UserSwitch1()
+        {
+            InitializeComponent();
+        }
+
+        public int UserWidth
+        {
+            get { return (int)GetValue(UserWidthProperty); }
+            set { SetValue(UserWidthProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for UserWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UserWidthProperty =
+            DependencyProperty.Register("UserWidth", typeof(int), typeof(UserSwitch1), new PropertyMetadata(0));
+
+        public int UserHeigth
+        {
+            get { return (int)GetValue(UserHeigthProperty); }
+            set { SetValue(UserHeigthProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for UserHeigth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UserHeigthProperty =
+            DependencyProperty.Register("UserHeigth", typeof(int), typeof(UserSwitch1), new PropertyMetadata(0));
+
+        public bool UserValue
+        {
+            get { return (bool)GetValue(UserValueProperty); }
+            set { SetValue(UserValueProperty, value); }
+        }
+        // Using a DependencyProperty as the backing store for UserValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UserValueProperty =
+            DependencyProperty.Register("UserValue", typeof(bool), typeof(UserSwitch1), new PropertyMetadata(false));
+        
+    }
+}
