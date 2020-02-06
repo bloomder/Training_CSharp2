dd924f861c6113dc5fb90bbd96bb35536e187e01
Rinat <bloomder@mail.ru>
2020-02-07 01:10:01 +0300

    add

--- SwithAnimation_WPF/Controls/UserSwitch.xaml.cs
+++ SwithAnimation_WPF/Controls/UserSwitch.xaml.cs
@@ -12,5 +12,54 @@ namespace SwithAnimation_WPF.Controls
         {
             InitializeComponent();
         }
+
+
+        public int GridHeigth
+        {
+            get { return (int)GetValue(GridHeigthProperty); }
+            set { SetValue(GridHeigthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GridHeigth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GridHeigthProperty =
+            DependencyProperty.Register("GridHeigth", typeof(int), typeof(UserSwitch), new PropertyMetadata(0));
+
+
+
+        public int GridWidth
+        {
+            get { return (int)GetValue(GridWidthProperty); }
+            set { SetValue(GridWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GridWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GridWidthProperty =
+            DependencyProperty.Register("GridWidth", typeof(int), typeof(UserSwitch), new PropertyMetadata(0));
+
+
+
+        public string RectColor
+        {
+            get { return (string)GetValue(RectColorProperty); }
+            set { SetValue(RectColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RectColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RectColorProperty =
+            DependencyProperty.Register("RectColor", typeof(string), typeof(UserSwitch), new PropertyMetadata(""));
+
+
+
+        public string RectFill
+        {
+            get { return (string)GetValue(RectFillProperty); }
+            set { SetValue(RectFillProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RectFill.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RectFillProperty =
+            DependencyProperty.Register("RectFill", typeof(string), typeof(UserSwitch), new PropertyMetadata(""));
+
+
     }
 }
